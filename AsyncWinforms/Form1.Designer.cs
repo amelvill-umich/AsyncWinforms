e77edff94674dbac305e8901ea325a134d2f28c3
ajm <ajm>
2021-03-10 14:07:11 -0500

    Initial commit

---
+++ AsyncWinforms/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace AsyncWinforms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRunAsync = new System.Windows.Forms.Button();
+            this.rtbText = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnRunAsync
+            // 
+            this.btnRunAsync.Location = new System.Drawing.Point(26, 13);
+            this.btnRunAsync.Name = "btnRunAsync";
+            this.btnRunAsync.Size = new System.Drawing.Size(85, 32);
+            this.btnRunAsync.TabIndex = 0;
+            this.btnRunAsync.Text = "Run Async";
+            this.btnRunAsync.UseVisualStyleBackColor = true;
+            this.btnRunAsync.Click += new System.EventHandler(this.btnRunAsync_Click);
+            // 
+            // rtbText
+            // 
+            this.rtbText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbText.Location = new System.Drawing.Point(0, 61);
+            this.rtbText.Name = "rtbText";
+            this.rtbText.ReadOnly = true;
+            this.rtbText.Size = new System.Drawing.Size(800, 389);
+            this.rtbText.TabIndex = 1;
+            this.rtbText.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnRunAsync);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 61);
+            this.panel1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtbText);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRunAsync;
+        private System.Windows.Forms.RichTextBox rtbText;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ AsyncWinforms/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * So async is really like an invisible callback, every time you put await in your code it's like saying
+ * Task.Run().then(... some code)
+ * 
+ * So 
+ * Console.WriteLine("Starting a task");
+ * await someTask;
+ * Console.WriteLine("Task is done");
+ * 
+ * is like saying
+ * 
+ * Console.WriteLine("Starting a task");
+ * someTask.Run(), then
+ * {
+ *      Console.WriteLine("Task is done.");
+ * }
+ */
+
+namespace AsyncWinforms
+{
+    public partial class Form1 : Form
+    {
+        public void WriteLine(string str)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action<string>(WriteLine), str);
+                return;
+            }
+
+            rtbText.AppendText($"{str}{Environment.NewLine}");
+            
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async Task<CToast> ToastBreadAsync(int slices)
+        {
+            for (int slice = 0; slice < slices; slice++)
+            {
+                WriteLine("Putting a slice of bread in the toaster");
+            }
+
+            WriteLine("Start toasting...");
+
+            // to try and illustrate the concept I put in unnecessary curly braces delimiting where the callbacks would be
+
+            // (return)
+            await Task.Delay(3000); // then () =>
+            {
+                WriteLine("Remove toast from toaster");
+
+                return new CToast(slices);
+            }
+        }
+
+        private async Task<CBacon> FryBaconAsync(int slices)
+        {
+            WriteLine($"putting {slices} slices of bacon in the pan");
+            WriteLine("cooking first side of bacon...");
+
+            await Task.Delay(3000); // then () =>
+            {
+                for (int slice = 0; slice < slices; slice++)
+                {
+                    WriteLine("flipping a slice of bacon");
+                }
+                WriteLine("cooking the second side of bacon...");
+
+                await Task.Delay(3000); // then () =>
+                {
+                    WriteLine("Put bacon on plate");
+
+                    return new CBacon(slices);
+                }
+            }
+        }
+
+        async Task<Sandwich> MakeBLT()
+        {
+            Task<CToast> toastTask = ToastBreadAsync(2);
+            Task<CBacon> baconTask = FryBaconAsync(3);
+
+            await Task.WhenAll(toastTask, baconTask); // then (toast, bacon) =>
+            {
+                return new Sandwich(toastTask.Result, baconTask.Result);
+            }
+        }
+
+        async Task PrintResult(Task<Sandwich> sandwichTask)
+        {
+            Sandwich sandwich = await sandwichTask; // then (sandwich) =>
+            {
+                WriteLine($"Your {sandwich.ToString()} is done!");
+            }
+        }
+
+        void WrongWay()
+        {
+            // this will be a deadlock, there's actually only one thread. The async methods run on the same thread!
+            Task<Sandwich> sandwichTask = MakeBLT();
+            TaskAwaiter<Sandwich> awaiter = sandwichTask.GetAwaiter();
+            Sandwich result = awaiter.GetResult();
+            
+       
+        }
+
+        private void btnRunAsync_Click(object sender, EventArgs e)
+        {
+            // the thing is, the first question on anyone's mind is how to run an async method from a non-async one.
+            // otherwise async will infect your whole codebase.
+            //
+            // the blogs say that you should just let that happen but nobody told you what happens if you can't make the switch 100% right now
+
+            Task<Sandwich> sandwichTask = MakeBLT();
+
+
+            // The Javascripty way to then print the result would be something like
+            // when done (sandwich) => { ... (the rest of the function)
+
+            // I could do that actually
+            Task printTask = PrintResult(sandwichTask);
+            WriteLine("OK the button handler is all done now.");
+
+            // we don't need to wait, and we shouldn't. We specified what gets ran when the task is done using PrintResult
+
+
+
+            // though, there's some other weirder cases worth investigating, like in JS, you can do something like print(await GetResult())
+            // and it will wait to call print until the result is there.
+        }
+
+    }
+
+    public class CToast
+    {
+        public int Slices { get; }
+        public CToast(int iSlices)
+        {
+            this.Slices = iSlices;
+        }
+    }
+
+    public class CBacon
+    {
+        public int Slices { get; }
+
+        public CBacon(int iSlices)
+        {
+            this.Slices = iSlices;
+        }
+    }
+
+    public class Sandwich
+    {
+        public CToast Toast;
+        public CBacon Bacon;
+        public Sandwich(CToast iToast, CBacon iBacon)
+        {
+            this.Toast = iToast;
+            this.Bacon = iBacon;
+        }
+
+        public override string ToString()
+        {
+            return $"Sandwich with {this.Toast.Slices} pieces of toast and {this.Bacon.Slices} pieces of bacon";
+        }
+    }
+}
